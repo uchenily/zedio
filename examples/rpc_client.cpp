6b2815b738dfa6e5e4cb51e36b79a8f197488176
uchenily <uchenily@qq.com>
2024-04-15 14:34:52 +0800

    rpc client 4

--- examples/rpc_client.cpp
+++ examples/rpc_client.cpp
@@ -23,6 +23,79 @@ using namespace zedio::net;
 using namespace zedio::async;
 using namespace zedio;
 
+struct RpcRequest {
+    std::vector<char> methods;
+
+    auto write_to(std::vector<char> &buf) {
+        buf.push_back('a');
+        buf.push_back(static_cast<char>(methods.size()));
+        buf.insert(buf.end(), methods.begin(), methods.end());
+    }
+};
+
+struct RpcResponse {
+    uint8_t chosen_method;
+
+    auto write_to(std::vector<char> &buf) const {
+        buf.push_back('a');
+        buf.push_back(static_cast<char>(chosen_method));
+    }
+};
+
+class RpcCodec {
+public:
+    auto encode(const RpcResponse &message) -> std::vector<char> {
+        std::vector<char> buf;
+        message.write_to(buf);
+        return buf;
+    }
+    auto decode([[maybe_unused]] std::span<char> buf) -> RpcRequest {
+        return RpcRequest{};
+    }
+};
+
+template <typename Codec>
+class Framed {
+public:
+    explicit Framed(TcpStream &&stream)
+        : stream_{std::move(stream)} {}
+
+public:
+    // 读取一个完整的数据帧
+    template <typename FrameType>
+    auto read_frame(std::vector<char> &buf) -> Task<Result<FrameType>> {
+        // 读取数据
+        co_await stream_.read(buf);
+        // 编码数据
+        auto res = codec_.decode(buf);
+        co_return res;
+    }
+
+    // 写入一个完整的数据帧
+    template <typename FrameType>
+        requires requires(FrameType msg, std::vector<char> &buf) { msg.write_to(buf); }
+    auto write_frame(FrameType &message) -> Task<void> {
+        // 编码数据
+        [[maybe_unused]] auto encoded = codec_.encode(message);
+        // 写入数据
+
+        // TODO: 添加一个模板方法: 一个类实现了 write_to(buf) 成员方法就可以调用
+        // stream_.write(encoded);
+        co_await stream_.write(encoded);
+        co_return;
+    }
+
+    // auto take_stream() -> TcpStream {
+    //     return std::move(stream_);
+    // }
+
+private:
+    Codec     codec_{};
+    TcpStream stream_;
+};
+
+using RpcFramed = Framed<RpcCodec>;
+
 class RpcClient {
 private:
     explicit RpcClient(TcpStream &&stream)
