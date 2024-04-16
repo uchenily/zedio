593af4b6903526ef8c6717cf0758f5987da5bdd3
uchenily <uchenily@qq.com>
2024-04-16 09:22:29 +0800

    codec move into rpc_util

--- examples/rpc_client.cpp
+++ examples/rpc_client.cpp
@@ -15,98 +15,15 @@
 #include <string_view>
 #include <utility>
 
-#include "rpc_util.hpp"
-
 using namespace zedio::log;
 using namespace zedio::io;
 using namespace zedio::net;
 using namespace zedio::async;
 using namespace zedio;
 
-struct RpcRequest {
-    std::string_view method;
+#include "rpc_util.hpp"
 
-    auto write_to(std::string &buf) const {
-        std::array<unsigned char, 4> msg_len{};
-        uint32_t                     length = method.size();
-
-        msg_len[3] = length & 0xFF;
-        msg_len[2] = (length >> 8) & 0xFF;
-        msg_len[1] = (length >> 16) & 0xFF;
-        msg_len[0] = (length >> 24) & 0xFF;
-        buf.append(std::string_view{reinterpret_cast<char *>(msg_len.data()), msg_len.size()});
-        buf.append(method);
-    }
-};
-
-struct RpcResponse {
-    std::string_view payload;
-};
-
-class RpcCodec {
-public:
-    auto encode(const RpcRequest &message) -> std::string {
-        std::string buf;
-        message.write_to(buf);
-        return buf;
-    }
-    auto decode([[maybe_unused]] std::span<char> buf) -> Result<RpcResponse> {
-        if (buf.size() < 4uz) {
-            return std::unexpected{make_zedio_error(Error::Unknown)};
-        }
-        auto length = buf[0] << 24 | buf[1] << 16 | buf[2] << 8 | buf[3];
-
-        if (buf.size() < 4uz + length) {
-            return std::unexpected{make_zedio_error(Error::Unknown)};
-        }
-
-        return RpcResponse{
-            std::string_view{buf.begin() + 4uz, buf.begin() + 4uz + length}
-        };
-    }
-};
-
-template <typename Codec>
-class Framed {
-public:
-    explicit Framed(TcpStream &&stream)
-        : stream_{std::move(stream)} {}
-
-public:
-    // 读取一个完整的数据帧
-    template <typename FrameType>
-    auto read_frame(std::vector<char> &buf) -> Task<Result<FrameType>> {
-        // 读取数据
-        co_await stream_.read(buf);
-        // 解码数据
-        auto res = codec_.decode(buf);
-        co_return res;
-    }
-
-    // 写入一个完整的数据帧
-    template <typename FrameType>
-        requires requires(FrameType msg, std::string &buf) { msg.write_to(buf); }
-    auto write_frame(FrameType &message) -> Task<void> {
-        // 编码数据
-        auto encoded = codec_.encode(message);
-        // 写入数据
-
-        // TODO: 添加一个模板方法: 一个类实现了 write_to(buf) 成员方法就可以调用
-        // stream_.write(encoded);
-        co_await stream_.write(encoded);
-        co_return;
-    }
-
-    // auto take_stream() -> TcpStream {
-    //     return std::move(stream_);
-    // }
-
-private:
-    Codec     codec_{};
-    TcpStream stream_;
-};
-
-using RpcFramed = Framed<RpcCodec>;
+using RpcFramed = Framed<RpcCodec<RpcRequest, RpcResponse>>;
 
 class RpcClient {
 private:
--- examples/rpc_util.hpp
+++ examples/rpc_util.hpp
@@ -1,9 +1,110 @@
 #pragma once
 
+// C++
+#include <array>
 #include <iostream>
 #include <sstream>
 #include <string>
 #include <string_view>
+
+// C
+#include <cstdint>
+
+struct RpcRequest {
+    std::string_view method;
+
+    auto write_to(std::string &buf) const {
+        std::array<unsigned char, 4> msg_len{};
+        uint32_t                     length = method.size();
+
+        msg_len[3] = length & 0xFF;
+        msg_len[2] = (length >> 8) & 0xFF;
+        msg_len[1] = (length >> 16) & 0xFF;
+        msg_len[0] = (length >> 24) & 0xFF;
+        buf.append(std::string_view{reinterpret_cast<char *>(msg_len.data()), msg_len.size()});
+        buf.append(method);
+    }
+};
+
+struct RpcResponse {
+    std::string_view payload;
+
+    auto write_to(std::string &buf) {
+        std::array<unsigned char, 4> msg_len{};
+        uint32_t                     length = payload.size();
+
+        msg_len[3] = length & 0xFF;
+        msg_len[2] = (length >> 8) & 0xFF;
+        msg_len[1] = (length >> 16) & 0xFF;
+        msg_len[0] = (length >> 24) & 0xFF;
+        buf.append(std::string_view{reinterpret_cast<char *>(msg_len.data()), msg_len.size()});
+        buf.append(payload);
+    }
+};
+
+template <typename MessageType1, typename MessageType2>
+class RpcCodec {
+public:
+    auto encode(MessageType1 &message) -> std::string {
+        std::string buf;
+        message.write_to(buf);
+        return buf;
+    }
+    auto decode([[maybe_unused]] std::span<char> buf) -> Result<MessageType2> {
+        if (buf.size() < 4uz) {
+            return std::unexpected{make_zedio_error(Error::Unknown)};
+        }
+        auto length = buf[0] << 24 | buf[1] << 16 | buf[2] << 8 | buf[3];
+
+        if (buf.size() < 4uz + length) {
+            return std::unexpected{make_zedio_error(Error::Unknown)};
+        }
+
+        return MessageType2{
+            std::string_view{buf.begin() + 4uz, buf.begin() + 4uz + length}
+        };
+    }
+};
+
+template <typename Codec>
+class Framed {
+public:
+    explicit Framed(TcpStream &&stream)
+        : stream_{std::move(stream)} {}
+
+public:
+    // 读取一个完整的数据帧
+    template <typename FrameType>
+    auto read_frame(std::vector<char> &buf) -> Task<Result<FrameType>> {
+        // 读取数据
+        co_await stream_.read(buf);
+        // 解码数据
+        auto res = codec_.decode(buf);
+        co_return res;
+    }
+
+    // 写入一个完整的数据帧
+    template <typename FrameType>
+        requires requires(FrameType msg, std::string &buf) { msg.write_to(buf); }
+    auto write_frame(FrameType &message) -> Task<void> {
+        // 编码数据
+        auto encoded = codec_.encode(message);
+        // 写入数据
+
+        // TODO: 添加一个模板方法: 一个类实现了 write_to(buf) 成员方法就可以调用
+        // stream_.write(encoded);
+        co_await stream_.write(encoded);
+        co_return;
+    }
+
+    // auto take_stream() -> TcpStream {
+    //     return std::move(stream_);
+    // }
+
+private:
+    Codec     codec_{};
+    TcpStream stream_;
+};
 
 class Person {
 public:
