d950dd42aaba5fc5acf762253a2d772cba0a7cbb
uchenily <uchenily@qq.com>
2024-04-16 12:20:04 +0800

    rpc server 11

--- examples/rpc_server.cpp
+++ examples/rpc_server.cpp
@@ -42,38 +42,55 @@ auto process(TcpStream stream) -> Task<void> {
     co_await rpc_framed.write_frame<RpcMessage>(resp);
 }
 
-auto server() -> Task<void> {
-    std::string host = "127.0.0.1";
-    uint16_t    port = 9000;
+class RpcServer {
+public:
+    RpcServer(std::string_view host, uint16_t port)
+        : host_{host}
+        , port_{port} {}
 
-    auto has_addr = SocketAddr::parse("127.0.0.1", 9000);
-    if (!has_addr) {
-        console.error(has_addr.error().message());
-        co_return;
-    }
-    auto has_listener = TcpListener::bind(has_addr.value());
-    if (!has_listener) {
-        console.error(has_listener.error().message());
-        co_return;
-    }
-    console.info("Listening on {}:{} ...", host, port);
-    auto listener = std::move(has_listener.value());
-    while (true) {
-        auto has_stream = co_await listener.accept();
+public:
+    void register_handler() {}
 
-        if (has_stream) {
-            auto &[stream, peer_addr] = has_stream.value();
-            console.info("Accept a connection from {}", peer_addr);
-            spawn(process(std::move(stream)));
-        } else {
-            console.error(has_stream.error().message());
-            break;
-        }
+    void run() {
+        auto runtime = Runtime::create();
+        runtime.block_on([]() -> Task<void> {
+            std::string host = "127.0.0.1";
+            uint16_t    port = 9000;
+
+            auto has_addr = SocketAddr::parse("127.0.0.1", 9000);
+            if (!has_addr) {
+                console.error(has_addr.error().message());
+                co_return;
+            }
+            auto has_listener = TcpListener::bind(has_addr.value());
+            if (!has_listener) {
+                console.error(has_listener.error().message());
+                co_return;
+            }
+            console.info("Listening on {}:{} ...", host, port);
+            auto listener = std::move(has_listener.value());
+            while (true) {
+                auto has_stream = co_await listener.accept();
+
+                if (has_stream) {
+                    auto &[stream, peer_addr] = has_stream.value();
+                    console.info("Accept a connection from {}", peer_addr);
+                    spawn(process(std::move(stream)));
+                } else {
+                    console.error(has_stream.error().message());
+                    break;
+                }
+            }
+        }());
     }
-}
+
+private:
+    std::string host_;
+    uint16_t    port_;
+};
 
 auto main() -> int {
     SET_LOG_LEVEL(zedio::log::LogLevel::Debug);
-    auto runtime = Runtime::create();
-    runtime.block_on(server());
+    RpcServer server{"127.0.0.1", 9000};
+    server.run();
 }
